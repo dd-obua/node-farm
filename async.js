24d4e7f83916a7414774912dae6be171c3d657e3
Obua Denis Daniel <dd.obua@gmail.com>
2024-08-08 13:14:34 +0300

    Read files asynchronously

--- async.js
+++ async.js
@@ -4,8 +4,13 @@ const fs = require('fs');
 const path = './txt/start.txt';
 const encoding = 'utf-8';
 
-fs.readFile(path, encoding, (error, data) => {
-  console.log(data);
+fs.readFile(path, encoding, (error, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, encoding, (error, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, encoding, (error, data3) => {
+      console.log(data3);
+    });
+  });
 });
 
 console.log('Will read file');
