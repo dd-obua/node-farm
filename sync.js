07d08883a06f3e99f3a455285b4e78d84a4692f5
Obua <imac@macs-MacBook-Air.local>
2024-08-08 11:49:17 +0300

    Write file asynchronously

--- sync.js
+++ sync.js
@@ -5,3 +5,9 @@ const encoding = 'utf-8';
 const textIn = fs.readFileSync(path, encoding);
 
 console.log(textIn);
+
+const writeTo = './txt/output.txt';
+const textOut = `This is what we know about the avocado: ${textIn}\nCreated at ${Date.now()}`;
+
+fs.writeFileSync(writeTo, textOut);
+console.log('File written');
