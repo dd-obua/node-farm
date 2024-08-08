38a333b573807d2f34d6faac34aaf6e80b72677d
Obua Denis Daniel <dd.obua@gmail.com>
2024-08-08 14:35:28 +0300

    Configure router

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const http = require('http');
 
 // Create server
 const server = http.createServer((req, res) => {
-  res.end('Hello from the server');
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('Overview page');
+  } else if (pathName === '/product') {
+    res.end('Product page');
+  }
 });
 
 // Start server
 const port = 8000;
 const host = '127.0.0.1';
+
 server.listen(port, host, () => {
   console.log(`Listing to request on port ${port}`);
 });
