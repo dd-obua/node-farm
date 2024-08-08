802a5b736257eae46270eb0dc9d05e5640d18503
Obua Denis Daniel <dd.obua@gmail.com>
2024-08-08 13:20:22 +0300

    Write data to a new file asynchronously

--- async.js
+++ async.js
@@ -9,6 +9,12 @@ fs.readFile(path, encoding, (error, data1) => {
     console.log(data2);
     fs.readFile(`./txt/append.txt`, encoding, (error, data3) => {
       console.log(data3);
+
+      // Write file asynchronously
+      const content = `${data2}\n${data3}`;
+      fs.writeFile(`./txt/final.txt`, content, encoding, (error) => {
+        console.log('Your file has been written');
+      });
     });
   });
 });
